dce1efc24d5e4a7377dae0992c2d214fce82e25e
Daneric <danericaalbers@hotmail.com>
2023-05-11 13:33:52 +0200

    feat: checkersboard

--- src/components/CheckersBoard.vue
+++ src/components/CheckersBoard.vue
@@ -1,79 +1,93 @@
 <template>
-    <div class="checkers-board">
-      <div v-for="(row, y) in board" :key="y" class="checkers-row">
-        <div v-for="(piece, x) in row" :key="getKey(x, y)" class="checkers-cell">
-          <div v-if="piece" class="checkers-piece" :class="{ 'red': piece.color === 'red', 'black': piece.color === 'black', 'king': piece.isKing }"></div>
+  <div class="checkerboard-container">
+    <div class="checkerboard-header">
+      <h1>Martvdm</h1>
+      <h2>Game Pin: 000000</h2>
+    </div>
+    <div class="checkerboard">
+      <div v-for="row in rows" :key="row">
+        <div class="checkerboard__square" v-for="col in cols" :key="col" :class="{'checkerboard__square--dark': isDarkSquare(row, col)}">
+          <div class="stone" v-if="isDarkSquare(row, col) && shouldAddStone(row, col)"></div>
         </div>
       </div>
     </div>
+  </div>
 </template>
-  
-<script lang="ts">
-  interface Piece {
-    color: string;
-    isKing: boolean;
-  }
-  
-  interface Board {
-    [x: number]: {
-      [y: number]: Piece | null;
-    };
-  }
-  
-  export default {
-    name: 'CheckersBoard',
-    props: {
-      board: {
-        type: Object as () => Board,
-        required: true
-      }
-    },
-    methods: {
-      getKey(x: number, y: number): string {
-        return `${x}-${y}`;
-      }
+
+<script>
+export default {
+  name: "CheckersBoard",
+  data() {
+    return {
+      rows: 8,
+      cols: 8,
     }
-  };
+  },
+  methods: {
+    isDarkSquare(row, col) {
+      // Determine if the square should be dark or light
+      return (row + col) % 2 === 0
+    },
+    shouldAddStone(row, col) {
+      // Determine if a stone should be added to the square
+      // Customize this logic based on your requirements
+      // For example, you can check specific row and column conditions
+      return (row === 0 || row === 2) && (col % 2 !== 0);
+    },
+  },
+}
 </script>
-  
-<style scoped>
-  .checkers-board {
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .checkers-row {
-    display: flex;
-  }
-  
-  .checkers-cell {
-    width: 50px;
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .checkers-piece {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: white;
-    border: 2px solid black;
-  }
-  
-  .checkers-piece.red {
-    background-color: red;
-  }
-  
-  .checkers-piece.black {
-    background-color: black;
-  }
-  
-  .checkers-piece.king:before {
-    content: "K";
-    font-weight: bold;
-    font-size: 30px;
-  }
+
+<style>
+.checkerboard-container {
+  display: flex;
+  flex-direction: column;
+  background-color: #D9D9D9;
+  width: 640px;
+  border-radius: 10px 10px 0 0;
+  border: 2px solid #ed135d;
+  font-family: 'Roboto', sans-serif;
+}
+
+.checkerboard-header {
+  border-bottom: 5px solid #ed135d;
+  padding: 0 10px 0 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  color: #ed135d;
+}
+
+.checkerboard-header a {
+  color: #f34981;
+  text-decoration: none;
+}
+
+.checkerboard {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  background-color: #D9D9D9;
+  font-family: 'Roboto', sans-serif;
+}
+
+.checkerboard__square {
+  width: 80px;
+  height: 80px;
+  background-color: #fff;
+}
+
+.checkerboard__square--dark {
+  background-color: #373737;
+}
+
+.stone {
+  background-color: #000; /* Customize the stone color */
+  width: 20px; /* Customize the stone size */
+  height: 20px;
+  border-radius: 50%;
+  margin: 5px; /* Customize the stone spacing */
+}
 </style>
-  
